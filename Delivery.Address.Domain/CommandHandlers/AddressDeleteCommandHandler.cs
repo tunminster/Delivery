758e1e6cc114404c30ab1764f235f08814cf1fa6
tminhein <tminhein@gmail.com>
2020-10-28 23:16:41 +0000

    added address command handlers

--- Delivery.Address.Domain/CommandHandlers/AddressDeleteCommandHandler.cs
+++ Delivery.Address.Domain/CommandHandlers/AddressDeleteCommandHandler.cs
@@ -1,14 +1,35 @@
 using System.Threading.Tasks;
 using Delivery.Address.Domain.Contracts;
+using Delivery.Database.Context;
 using Delivery.Domain.CommandHandlers;
 
 namespace Delivery.Address.Domain.CommandHandlers
 {
     public class AddressDeleteCommandHandler : ICommandHandler<AddressDeleteCommand, AddressDeleteStatusContract>
     {
-        public Task<AddressDeleteStatusContract> Handle(AddressDeleteCommand command)
+        private readonly ApplicationDbContext applicationDbContext;
+        
+        public AddressDeleteCommandHandler (ApplicationDbContext applicationDbContext)
         {
-            throw new System.NotImplementedException();
+            this.applicationDbContext = applicationDbContext;
+        }
+        
+        public async Task<AddressDeleteStatusContract> Handle(AddressDeleteCommand command)
+        {
+            
+            var address = await applicationDbContext.Addresses.FindAsync(command.AddressId);
+            var addressDeleteStatusContract = new AddressDeleteStatusContract();
+            
+            if (address == null)
+            {
+                return addressDeleteStatusContract;
+            }
+            
+            applicationDbContext.Addresses.Remove(address);
+            await applicationDbContext.SaveChangesAsync();
+            addressDeleteStatusContract.AddressDeleted = true;
+
+            return addressDeleteStatusContract;
         }
     }
 }
--- Delivery.Address.Domain/CommandHandlers/AddressUpdateCommandHandler.cs
+++ Delivery.Address.Domain/CommandHandlers/AddressUpdateCommandHandler.cs
@@ -1,14 +1,43 @@
 using System.Threading.Tasks;
 using Delivery.Address.Domain.Contracts;
+using Delivery.Database.Context;
 using Delivery.Domain.CommandHandlers;
+using Microsoft.EntityFrameworkCore;
 
 namespace Delivery.Address.Domain.CommandHandlers
 {
     public class AddressUpdateCommandHandler : ICommandHandler<AddressUpdateCommand, AddressUpdateStatusContract>
     {
-        public Task<AddressUpdateStatusContract> Handle(AddressUpdateCommand command)
+        private readonly ApplicationDbContext applicationDbContext;
+        
+        public AddressUpdateCommandHandler (ApplicationDbContext applicationDbContext)
         {
-            throw new System.NotImplementedException();
+            this.applicationDbContext = applicationDbContext;
+        }
+        
+        public async Task<AddressUpdateStatusContract> Handle(AddressUpdateCommand command)
+        {
+            var address = await applicationDbContext.Addresses.FindAsync(command.AddressContract.Id);
+
+            var addressUpdateStatusContract = new AddressUpdateStatusContract();
+            if (address == null)
+            {
+                return addressUpdateStatusContract;
+            }
+
+            address.City = command.AddressContract.City;
+            address.Country = command.AddressContract.Country;
+            address.Description = command.AddressContract.Description;
+            address.Lat = command.AddressContract.Lat;
+            address.Lng = command.AddressContract.Lng;
+            address.AddressLine = command.AddressContract.AddressLine;
+            address.PostCode = command.AddressContract.PostCode;
+            
+            applicationDbContext.Entry(address).State = EntityState.Modified;
+            await applicationDbContext.SaveChangesAsync();
+            addressUpdateStatusContract.AddressUpdated = true;
+
+            return addressUpdateStatusContract;
         }
     }
 }
