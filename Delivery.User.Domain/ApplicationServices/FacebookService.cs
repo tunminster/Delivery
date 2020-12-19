d58ca62a7b0a755c6ede2722b3a3904fe67ac002
tminhein <tminhein@gmail.com>
2020-12-19 00:13:52 +0000

    fixed google to fetch email

--- Delivery.User.Domain/ApplicationServices/FacebookService.cs
+++ Delivery.User.Domain/ApplicationServices/FacebookService.cs
@@ -3,6 +3,8 @@ using System.Net.Http;
 using System.Net.Http.Headers;
 using System.Threading.Tasks;
 using Delivery.Azure.Library.Database.Context;
+using Delivery.Azure.Library.Exceptions.Extensions;
+using Delivery.Azure.Library.Sharding.Adapters;
 using Delivery.Domain.Factories;
 using Delivery.User.Domain.Contracts.Facebook;
 using Newtonsoft.Json;
@@ -12,12 +14,15 @@ namespace Delivery.User.Domain.ApplicationServices
     public class FacebookService
     {
         private readonly HttpClient httpClient;
+        private readonly IExecutingRequestContextAdapter executingRequestContextAdapter;
 
-        public FacebookService()
+        public FacebookService(IExecutingRequestContextAdapter executingRequestContextAdapter)
         {
             httpClient = new HttpClient{
                 BaseAddress = new Uri("https://graph.facebook.com/v2.8/")
             };
+
+            this.executingRequestContextAdapter = executingRequestContextAdapter;
             
             httpClient.DefaultRequestHeaders
                 .Accept
@@ -29,12 +34,13 @@ namespace Delivery.User.Domain.ApplicationServices
             var result = await GetAsync<dynamic>(facebookToken, "me", "fields=id,first_name,last_name,email,picture.width(100).height(100)");
             if (result == null)
             {
-                throw new Exception("User from this token not exist");
+                throw new InvalidOperationException("The provided facebook token returned null.User from this token does not exist")
+                    .WithTelemetry(executingRequestContextAdapter.GetTelemetryProperties());
             }
 
             var account = new FacebookUserContract()
             {
-                Email = result.email ?? result.id,
+                Email = result.email,
                 FirstName = result.first_name,
                 LastName = result.last_name,
                 Picture = result.picture.data.url
--- Delivery.User.Domain/ApplicationServices/GoogleService.cs
+++ Delivery.User.Domain/ApplicationServices/GoogleService.cs
@@ -1,8 +1,12 @@
 using System;
+using System.Collections.Generic;
+using System.Dynamic;
+using System.Net;
 using System.Net.Http;
 using System.Net.Http.Headers;
 using System.Threading.Tasks;
 using Delivery.Azure.Library.Database.Context;
+using Delivery.Azure.Library.Exceptions.Extensions;
 using Delivery.Azure.Library.Sharding.Adapters;
 using Delivery.Domain.Factories.Auth;
 using Delivery.Domain.Models;
@@ -11,18 +15,22 @@ using Delivery.User.Domain.Contracts.Facebook;
 using Delivery.User.Domain.Contracts.Google;
 using Microsoft.AspNetCore.Identity;
 using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
 
 namespace Delivery.User.Domain.ApplicationServices
 {
     public class GoogleService
     {
         private readonly HttpClient httpClient;
+        private readonly IExecutingRequestContextAdapter executingRequestContextAdapter;
 
-        public GoogleService()
+        public GoogleService(IExecutingRequestContextAdapter executingRequestContextAdapter)
         {
             httpClient = new HttpClient{
                 BaseAddress = new Uri("https://oauth2.googleapis.com/")
             };
+
+            this.executingRequestContextAdapter = executingRequestContextAdapter;
             
             httpClient.DefaultRequestHeaders
                 .Accept
@@ -34,14 +42,14 @@ namespace Delivery.User.Domain.ApplicationServices
             var result = await GetAsync<dynamic>(idToken, "tokeninfo");
             if (result == null)
             {
-                throw new Exception("User from this token not exist");
+                throw new InvalidOperationException("User from this token does not exist");
             }
-
+            
             var account = new GoogleUserContract
             {
                 Iss = result.iss,
                 Sub = result.sub,
-                Email = result.email ?? result.id,
+                Email = result.email ?? throw new InvalidOperationException("Email scope was not provided from the Id Token"),
                 EmailVerified = result.email_verified == "true",
                 Name = result.name,
                 Picture = result.picture,
@@ -51,6 +59,7 @@ namespace Delivery.User.Domain.ApplicationServices
             };
 
             return account;
+            
         }
         
 
@@ -58,12 +67,16 @@ namespace Delivery.User.Domain.ApplicationServices
         {
             var response = await httpClient.GetAsync($"{endpoint}?id_token={accessToken}");
             if (!response.IsSuccessStatusCode)
-                return default(T);
-
+            {
+                throw new InvalidOperationException("Google token api response is not succeeded.")
+                    .WithTelemetry(executingRequestContextAdapter.GetTelemetryProperties());
+            }
+            
             var result = await response.Content.ReadAsStringAsync();
 
             return JsonConvert.DeserializeObject<T>(result);
         }
+        
         
     }
 }
