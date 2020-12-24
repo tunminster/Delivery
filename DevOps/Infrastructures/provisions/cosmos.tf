6fec3ad7b7908369a399e4798e5188b9fe44c624
tminhein <tminhein@gmail.com>
2020-12-24 02:43:37 +0000

    fix

--- DevOps/Infrastructures/provisions/cosmos.tf
+++ DevOps/Infrastructures/provisions/cosmos.tf
@@ -1,5 +1,5 @@
 resource "azurerm_cosmosdb_account" "hn-platform-cosmos-db" {
-  name                = "hn-platform-cosmos-db-${var.environment_prefix}"
+  name                = "platform-cosmos-db-${var.environment_prefix}"
   location            = azurerm_resource_group.hn-platform-data-persistent.location
   resource_group_name = azurerm_resource_group.hn-platform-data-persistent.name
   offer_type          = "Standard"
@@ -25,7 +25,7 @@ resource "azurerm_cosmosdb_account" "hn-platform-cosmos-db" {
 }
 
 resource "azurerm_cosmosdb_sql_database" "db" {
-  name                = "hn-platform-cosmossql-${var.environment_prefix}"
+  name                = "platform-cosmossql-${var.environment_prefix}"
   resource_group_name = azurerm_cosmosdb_account.hn-platform-cosmos-db.resource_group_name
   account_name        = azurerm_cosmosdb_account.hn-platform-cosmos-db.name
 }
