fbbeaebe3b07e2f121e00a50fc381dfec63107dd
tminhein <tminhein@gmail.com>
2020-12-24 02:27:35 +0000

    fix

--- DevOps/Infrastructures/provisions/cosmos.tf
+++ DevOps/Infrastructures/provisions/cosmos.tf
@@ -1,7 +1,7 @@
 resource "azurerm_cosmosdb_account" "hn-platform-cosmos-db" {
   name                = "hn-platform-cosmos-db"
-  location            = resource.azurerm_resource_group.hn-platform-rg.location
-  resource_group_name = resource.azurerm_resource_group.hn-platform-rg.name
+  location            = azurerm_resource_group.hn-platform-data-persistent.location
+  resource_group_name = azurerm_resource_group.hn-platform-data-persistent.name
   offer_type          = "Standard"
   kind                    = "GlobalDocumentDB"
 
