d0c31ce57d914a545580c1ecae2b9f257ba3ccec
tminhein <tminhein@gmail.com>
2020-12-24 02:35:04 +0000

    fix

--- DevOps/Infrastructures/provisions/cosmos.tf
+++ DevOps/Infrastructures/provisions/cosmos.tf
@@ -1,12 +1,31 @@
 resource "azurerm_cosmosdb_account" "hn-platform-cosmos-db" {
-  name                = "hn-platform-cosmos-db"
+  name                = "hn-platform-cosmos-db-${var.environment_prefix}"
   location            = azurerm_resource_group.hn-platform-data-persistent.location
   resource_group_name = azurerm_resource_group.hn-platform-data-persistent.name
   offer_type          = "Standard"
   kind                    = "GlobalDocumentDB"
 
   enable_automatic_failover = false
+  
   capabilities {
     name = "EnableServerless"
   }
+
+  consistency_policy {
+    consistency_level       = "BoundedStaleness"
+    max_interval_in_seconds = 10
+    max_staleness_prefix    = 200
+  }
+
+  geo_location {
+    location          = azurerm_resource_group.rg.location
+    failover_priority = 0
+  }
+
+}
+
+resource "azurerm_cosmosdb_sql_database" "db" {
+  name                = "hn-platform-cosmossql-${var.environment_prefix}"
+  resource_group_name = azurerm_cosmosdb_account.db.resource_group_name
+  account_name        = azurerm_cosmosdb_account.db.name
 }
