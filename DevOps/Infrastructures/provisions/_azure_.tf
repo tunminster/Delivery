5d0fc04de2d597053e55bf466533a5a3d4cc4175
tminhein <tminhein@gmail.com>
2020-12-24 16:26:08 +0000

    fixed backend azurerm

--- DevOps/Infrastructures/provisions/_azure_.tf
+++ DevOps/Infrastructures/provisions/_azure_.tf
@@ -4,3 +4,13 @@ provider "azurerm" {
   version = "=2.40.0"
   features {}
 }
+
+terraform{
+  backend "azurerm" {
+    storage_account_name = "${var.state_storage_account_name}"
+    container_name       = "${var.state_storage_account_container_name}"
+    key                  = "${var.state_storage_key_file}"
+
+    access_key = "${var.state_storage_access_key}"
+  }
+}
