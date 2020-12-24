d27bdc27a369be6c1b92dd5f2576d0dc9928a8a6
tminhein <tminhein@gmail.com>
2020-12-24 01:27:43 +0000

    fixed

--- DevOps/Infrastructures/provisions/variables.tf
+++ DevOps/Infrastructures/provisions/variables.tf
@@ -1,17 +1,13 @@
 variable "state_storage_access_key" {
-    type = string
 }
 
 variable "state_storage_key_file" {
-    type = string
 }
 
 variable "state_storage_account_name" {
-    type = string
 }
 
 variable "state_storage_account_container_name" {
-    type = string
 }
 
 variable "environment_prefix" {
