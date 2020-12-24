7751370eb784e875a4d188e83a7bbfb9b6eb2794
tminhein <tminhein@gmail.com>
2020-12-24 01:34:55 +0000

    fix

--- DevOps/Infrastructures/provisions/init.tf
+++
@@ -1,8 +0,0 @@
-terraform {
-  backend "azurerm" {
-    storage_account_name = "${var.state_storage_account_name}"
-    container_name       = "${var.state_storage_account_container_name}"
-    key                  = "${var.state_storage_key_file}"
-    access_key = "${var.state_storage_access_key}"
-  }
-}
