e46780252096cd5edcb12fa91c37b432949274f1
tminhein <tminhein@gmail.com>
2020-12-25 01:47:16 +0000

    added cosmos saving

--- Delivery.StripePayment.Domain/CommandHandlers/AccountCreation/AccountCreationCommandHandler.cs
+++ Delivery.StripePayment.Domain/CommandHandlers/AccountCreation/AccountCreationCommandHandler.cs
@@ -1,13 +1,16 @@
 using System;
+using System.Collections.Generic;
 using System.Threading.Tasks;
 using Delivery.Azure.Library.Configuration.Configurations.Interfaces;
 using Delivery.Azure.Library.Sharding.Adapters;
 using Delivery.Azure.Library.Storage.Cosmos.Accessors;
 using Delivery.Azure.Library.Storage.Cosmos.Configurations;
+using Delivery.Azure.Library.Storage.Cosmos.Contracts;
 using Delivery.Azure.Library.Storage.Cosmos.Services;
 using Delivery.Domain.CommandHandlers;
 using Delivery.Domain.Constants;
 using Delivery.StripePayment.Domain.Contracts.Enums;
+using Delivery.StripePayment.Domain.Contracts.Models;
 using Delivery.StripePayment.Domain.Contracts.V1.RestContracts;
 using Microsoft.Extensions.DependencyInjection;
 using Stripe;
@@ -51,11 +54,29 @@ namespace Delivery.StripePayment.Domain.CommandHandlers.AccountCreation
             
             var service = new AccountService();
             var account = await service.CreateAsync(options);
+
+            var stripeAccountContract = new StripeAccountContract
+            {
+                AccountId = account.Id,
+                Shard = executingRequestContextAdapter.GetShard().Key
+            };
+
+            var stripeDocumentAccount = new DocumentContract<StripeAccountContract>
+            {
+                Id = Guid.NewGuid(),
+                PartitionKey = stripeAccountContract.Shard,
+                Data = new List<StripeAccountContract> {stripeAccountContract},
+                InsertedBy = executingRequestContextAdapter.GetAuthenticatedUser().UserEmail,
+                InsertionDate = DateTimeOffset.UtcNow,
+                ValidFromDate = DateTimeOffset.UtcNow,
+                Version = 1
+            };
             
-            //Todo: save into cosmosdb
+            //save into cosmosdb
             var cosmosDatabaseAccessor = await CosmosDatabaseAccessor.CreateAsync(serviceProvider, new CosmosDatabaseConnectionConfigurationDefinition(serviceProvider, Constants.CosmosDatabaseHnPlatformConnectionString));
             var platformCosmosDbService = new PlatformCosmosDbService(serviceProvider, executingRequestContextAdapter, cosmosDatabaseAccessor.CosmosClient, cosmosDatabaseAccessor.GetContainer(Constants.HnPlatform, ContainerConstants.ContainerConstants.ConnectAccountCollectionName));
-                
+            await new PlatformCachedCosmosDbService(serviceProvider, executingRequestContextAdapter, platformCosmosDbService).AddItemAsync<DocumentContract<StripeAccountContract>, StripeAccountContract>(stripeDocumentAccount);
+            
             var stripeAccountCreationStatusContract = new StripeAccountCreationStatusContract
             {
                 AccountId = account.Id,
