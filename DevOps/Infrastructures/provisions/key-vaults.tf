38ce656316589ff19e506075c4f600afe303eb19
tminhein <tminhein@gmail.com>
2020-12-30 13:47:02 +0000

    added service bus infra

---
+++ DevOps/Infrastructures/provisions/key-vaults.tf
@@ -0,0 +1,31 @@
+resource "azurerm_key_vault" "hn-platform-key-vault" {
+  name                = "hn-platform-key-vault-${var.environment_prefix}"
+  resource_group_name = azurerm_resource_group.hn-platform-data-persistent.name
+  location            = azurerm_resource_group.hn-platform-data-persistent.location
+  sku_name            = "standard"
+  tenant_id           = var.tenant_id
+  soft_delete_enabled = true
+  soft_delete_retention_days  = 7
+  tags = var.environment_prefix
+}
+
+resource "azurerm_key_vault_access_policy" "hn-contributor-access-policy" {
+  key_vault_id = azurerm_key_vault.hn-platform-key-vault.id
+  tenant_id    = var.tenant_id
+  object_id    = var.active_directory_contributor_object_id
+
+  certificate_permissions = [
+    "get",
+    "list"
+  ]
+
+  key_permissions = [
+    "get",
+    "list"
+  ]
+
+  secret_permissions = [
+    "get",
+    "list"
+  ]
+}
--- DevOps/Infrastructures/provisions/variables/secret.tfvars
+++ DevOps/Infrastructures/provisions/variables/secret.tfvars
@@ -3,5 +3,7 @@ state_storage_account_container_name =  "#{state_storage_account_container_name}
 state_storage_key_file = "#{state_storage_key_file}#"
 state_storage_access_key = "#{hn-platform-pngo-storage-secret-key}#"
 environment_prefix = "#{environment_prefix}#"
+tenant_id = "#{tenant_id}#"
+active_directory_contributor_object_id = "#{active_directory_contributor_object_id}#"
 
 
