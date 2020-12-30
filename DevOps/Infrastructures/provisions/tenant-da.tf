371f3ed25d11c7925211371adfd4708d934e7189
tminhein <tminhein@gmail.com>
2020-12-30 18:50:40 +0000

    fixed

--- DevOps/Infrastructures/provisions/tenant-da.tf
+++ DevOps/Infrastructures/provisions/tenant-da.tf
@@ -15,21 +15,6 @@ resource "azurerm_mssql_database" "da" {
     retention_in_days                       = 6
   }
 
-  resource "azurerm_sql_firewall_rule" "da" {
-    name                = "AlllowAzureServices"
-    resource_group_name = azurerm_resource_group.hn-platform-data-persistent.name
-    server_name         = azurerm_sql_server.hn-platform-sql-server.name
-    start_ip_address    = "0.0.0.0"
-    end_ip_address      = "0.0.0.0"
- }
-
-  resource "azurerm_sql_firewall_rule" "da" {
-    name                = "FirewallRule1"
-    resource_group_name = azurerm_resource_group.hn-platform-data-persistent.name
-    server_name         = azurerm_sql_server.hn-platform-sql-server.name
-    start_ip_address    = "81.109.71.197"
-    end_ip_address      = "81.109.71.197"
- }
 
   tags = local.common_tags
 
