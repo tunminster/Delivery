a208a1d10b8ff91e12cb6a8abb58b67b3fd67bc9
tminhein <tminhein@gmail.com>
2021-02-14 20:58:36 +0000

    fix public ip address

--- DevOps/Infrastructures/provisions/public-ip.tf
+++ DevOps/Infrastructures/provisions/public-ip.tf
@@ -1,6 +1,6 @@
 resource "azurerm_public_ip" "hn-public-ip" {
   name                = "hnPlatformIp"
-  resource_group_name = azurerm_resource_group.hn-platform-transitory.name
+  resource_group_name = "MC_hn-platform-transitory-dev_hn-platform-aks_eastus2"
   location            = azurerm_resource_group.hn-platform-transitory.location
   allocation_method   = "Static"
 
