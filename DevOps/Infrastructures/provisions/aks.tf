139895970c4ca05e2fe5e667231d228014ee309f
tminhein <tminhein@gmail.com>
2021-02-14 16:12:08 +0000

    fix os disk type

--- DevOps/Infrastructures/provisions/aks.tf
+++ DevOps/Infrastructures/provisions/aks.tf
@@ -12,7 +12,10 @@ resource "azurerm_kubernetes_cluster" "hn-platform-aks" {
     max_count             = 3
     vm_size               = var.aks_node_vm_size
     os_disk_size_gb       = var.aks_node_os_disk_size
-    os_disk_type          = "managed"
+    os_disk {
+      storage_account_type = "Standard_LRS"
+      caching              = "ReadWrite"
+    } 
   }
 
   service_principal {
