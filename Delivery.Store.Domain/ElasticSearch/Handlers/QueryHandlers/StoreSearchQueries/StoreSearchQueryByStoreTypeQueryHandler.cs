b1a2a88a378b098424827c61cf4a3d8b173e6467
tminhein <tminhein@gmail.com>
2021-02-16 22:32:23 +0000

    fix search store type

--- Delivery.Store.Domain/ElasticSearch/Handlers/QueryHandlers/StoreSearchQueries/StoreSearchQueryByStoreTypeQueryHandler.cs
+++ Delivery.Store.Domain/ElasticSearch/Handlers/QueryHandlers/StoreSearchQueries/StoreSearchQueryByStoreTypeQueryHandler.cs
@@ -2,45 +2,40 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-using Delivery.Azure.Library.Configuration.Configurations.Interfaces;
 using Delivery.Azure.Library.Sharding.Adapters;
 using Delivery.Domain.QueryHandlers;
 using Delivery.Store.Domain.Contracts.V1.ModelContracts;
-using Delivery.Store.Domain.Handlers.QueryHandlers.StoreGetQueries;
 using Microsoft.Extensions.DependencyInjection;
 using Nest;
 
 namespace Delivery.Store.Domain.ElasticSearch.Handlers.QueryHandlers.StoreSearchQueries
 {
-    public class StoreSearchQueryHandler : IQueryHandler<StoreSearchQuery, List<StoreContract>>
+    public class StoreSearchQueryByStoreTypeQueryHandler : IQueryHandler<StoreSearchQueryByStoreTypeQuery, List<StoreContract>>
     {
         private readonly IServiceProvider serviceProvider;
         private readonly IExecutingRequestContextAdapter executingRequestContextAdapter;
         
-        public StoreSearchQueryHandler(IServiceProvider serviceProvider, IExecutingRequestContextAdapter executingRequestContextAdapter)
+        public StoreSearchQueryByStoreTypeQueryHandler(IServiceProvider serviceProvider, IExecutingRequestContextAdapter executingRequestContextAdapter)
         {
             this.serviceProvider = serviceProvider;
             this.executingRequestContextAdapter = executingRequestContextAdapter;
         }
         
-        public async Task<List<StoreContract>> Handle(StoreSearchQuery query)
+        public async Task<List<StoreContract>> Handle(StoreSearchQueryByStoreTypeQuery query)
         {
             var elasticClient = serviceProvider.GetRequiredService<IElasticClient>();
             
             var currentLocation = new GeoLocation(query.Latitude, query.Longitude);
-            var storeTypeArr = query.StoreType != null ? query.StoreType.Split(new string[] {",", " & ", " "}, StringSplitOptions.None).ToArray() : new []{""};
             
             var searchResponse = await elasticClient.SearchAsync<StoreContract>(s => s
                 .AllIndices()
                 .Query(q => 
-                    q.QueryString(qs => 
-                        qs.Fields(f => 
-                            f.Field(x => x.StoreName).Field(x => x.StoreType)
-                        ).Query(query.QueryString))
-                    && q.Bool(b => 
-                            b.Filter(f => 
-                                f.Terms(tm => 
-                                    tm.Field(fd => fd.StoreType).Terms(storeTypeArr))))
+                    q.Match(m => 
+                        m.Field(f => 
+                            f.StoreType)
+                        .Query(query.StoreType)
+                        .Operator(Operator.And)
+                        .ZeroTermsQuery(ZeroTermsQuery.All))
                     && q.Bool(b => 
                         b.Filter(f => 
                             f.GeoDistance(g => 
@@ -51,14 +46,14 @@ namespace Delivery.Store.Domain.ElasticSearch.Handlers.QueryHandlers.StoreSearch
                                     .Location(currentLocation)
                                     .Distance(query.Distance) // Eg: "3km"
                                     .ValidationMethod(GeoValidationMethod.IgnoreMalformed)
-                                )))
+                            )))
                     
                 )
                 .Source()
                 .ScriptFields(sf => sf
                     .ScriptField("distance", descriptor => descriptor
                         .Source(script:$"doc[\u0027location\u0027].arcDistance({query.Latitude}, {query.Longitude}) * 0.001")
-                        ))
+                    ))
                 .From((query.Page - 1) * query.PageSize)
                 .Size(query.PageSize));
             
