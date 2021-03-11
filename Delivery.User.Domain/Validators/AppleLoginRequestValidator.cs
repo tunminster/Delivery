8ab8ea93108083a5c3d73ceacbd137e9a541a3b3
tminhein <tminhein@gmail.com>
2021-03-11 00:21:19 +0000

    fix applie login

--- Delivery.User.Domain/Validators/AppleLoginRequestValidator.cs
+++ Delivery.User.Domain/Validators/AppleLoginRequestValidator.cs
@@ -7,9 +7,6 @@ namespace Delivery.User.Domain.Validators
     {
         public AppleLoginRequestValidator()
         {
-            RuleFor(x => x.Email).NotNull().NotEmpty().WithMessage("Email name must be provided.");
-            RuleFor(x => x.FamilyName).NotNull().NotEmpty().WithMessage("Family name must be provided.");
-            RuleFor(x => x.GivenName).NotNull().NotEmpty().WithMessage("Given name must be provided.");
             RuleFor(x => x.AuthorizationCode).NotNull().NotEmpty().WithMessage("Authorization code must be provided.");
             RuleFor(x => x.IdentityToken).NotNull().NotEmpty().WithMessage("Identity token must be provided.");
         }
