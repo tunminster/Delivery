07b31952bae43e4fc10c64dac490d7ce67447829
tminhein <tminhein@gmail.com>
2021-08-28 10:48:03 +0100

    fix address to driver creation

--- Delivery.Driver.Domain/Contracts/V1/RestContracts/DriverCreationContract.cs
+++ Delivery.Driver.Domain/Contracts/V1/RestContracts/DriverCreationContract.cs
@@ -10,73 +10,123 @@ namespace Delivery.Driver.Domain.Contracts.V1.RestContracts
     {
         /// <summary>
         ///  Driver full name
+        /// <example>{{fullName}}</example>
         /// </summary>
         public string FullName { get; init; } = string.Empty;
 
         /// <summary>
         ///  Driver Email address
+        /// <example>{{emailAddress}}</example>
         /// </summary>
         public string EmailAddress { get; init; } = string.Empty;
         
         /// <summary>
         /// Driver vehicle type
+        /// <example>{{vehicleType}}</example>
         /// </summary>
         public VehicleType VehicleType { get; init; }
 
         /// <summary>
         ///  Driver's license number
+        /// <example>{{drivingLicenseNumber}}</example>
         /// </summary>
         public string DrivingLicenseNumber { get; init; } = string.Empty;
         
         /// <summary>
         ///  Driver's social security number
+        /// <example>{{socialSecurityNumber}}</example>
         /// </summary>
         public string SocialSecurityNumber { get; init; } = string.Empty;
         
         /// <summary>
         ///  Driver license expiry date
+        /// <example>{{drivingLicenseExpiryDate}}</example>
         /// </summary>
         public DateTimeOffset DrivingLicenseExpiryDate { get; init; }
 
         /// <summary>
         ///  Driver bank name
+        /// <example>{{bankName}}</example>
         /// </summary>
         public string BankName { get; init; } = string.Empty;
 
         /// <summary>
         ///  Driver bank account number
+        /// <example>{{bankAccountNumber}}</example>
         /// </summary>
         public string BankAccountNumber { get; init; } = string.Empty;
 
         /// <summary>
         ///  Bank routing number
+        /// <example>{{routingNumber}}</example>
         /// </summary>
         public string RoutingNumber { get; init; } = string.Empty;
 
         /// <summary>
         ///  Driver's service area that he/she intends to work.
+        /// <example>{{serviceArea}}</example>
         /// </summary>
         public string ServiceArea { get; init; } = string.Empty;
         
         /// <summary>
         ///  Radius that would cover to give delivery service
+        /// <example>{{radius}}</example>
         /// </summary>
         public int Radius { get; init; }
         
         /// <summary>
-        ///  Service area latitude
+        ///  Service area latitude that driver wants to work
+        /// <example>{{latitude}}</example>
         /// </summary>
         public double Latitude { get; set; }
         
         /// <summary>
-        ///  Service area longitude
+        ///  Service area longitude that driver wants to work
+        /// <example>{{longitude}}</example>
         /// </summary>
         public double Longitude { get; set; }
 
         /// <summary>
         ///  Driver user account password
+        /// <example>{{password}}</example>
         /// </summary>
         public string Password { get; init; } = string.Empty;
+        
+        /// <summary>
+        ///  Driver user account confirm password
+        /// <example>{{password}}</example>
+        /// </summary>
+        public string ConfirmPassword { get; init; } = string.Empty;
+
+        /// <summary>
+        ///  Address line 1
+        /// <example>{{addressLine1}}</example>
+        /// </summary>
+        public string AddressLine1 { get; init; } = string.Empty;
+        
+        /// <summary>
+        ///  Address line 2
+        /// <example>{{addressLine2}}</example>
+        /// </summary>
+        public string AddressLine2 { get; init; } = string.Empty;
+        
+        /// <summary>
+        ///  City
+        /// <example>{{city}}</example>
+        /// </summary>
+        public string City { get; init; } = string.Empty;
+        
+        /// <summary>
+        ///  County
+        /// <example>{{county}}</example>
+        /// </summary>
+        public string County { get; init; } = string.Empty;
+        
+        /// <summary>
+        ///  Country
+        /// <example>{{country}}</example>
+        /// </summary>
+        public string Country { get; init; } = string.Empty;
 
     }
 }
--- Delivery.Driver.Domain/Validators/DriverCreationValidator.cs
+++ Delivery.Driver.Domain/Validators/DriverCreationValidator.cs
@@ -12,13 +12,19 @@ namespace Delivery.Driver.Domain.Validators
             RuleFor(x => x.BankName).NotEmpty().NotNull().WithMessage("Bank name must be provided.");
             RuleFor(x => x.Password).NotEmpty().NotNull().WithMessage("Password must be valid.");
             RuleFor(x => x.Password).MinimumLength(6).WithMessage("Password must be at least 6 characters");
+            RuleFor(x => x.ConfirmPassword).NotEmpty().NotNull().WithMessage("Confirm password must be provided");
+            RuleFor(x => x.Password).Equal(x => x.ConfirmPassword).WithMessage("Passwords do not match.");
             RuleFor(x => x.Radius).NotNull().GreaterThanOrEqualTo(5)
                 .WithMessage("Radius area must be covered 5 miles minimum.")
                 .LessThanOrEqualTo(20).WithMessage("Radius area must be covered maximum 20 miles.");
             RuleFor(x => x.ServiceArea).NotEmpty().NotNull().WithMessage("Must provide valid service area");
             RuleFor(x => x.Latitude).NotEqual(0).WithMessage("Must provide valid latitude.");
             RuleFor(x => x.Longitude).NotEqual(0).WithMessage("Must provide valid longitude.");
-
+            RuleFor(x => x.AddressLine1).NotEmpty().NotNull().WithMessage("AddressLine1 must be provided");
+            RuleFor(x => x.City).NotEmpty().NotNull().WithMessage("City must be provided");
+            RuleFor(x => x.County).NotEmpty().NotNull().WithMessage("County must be provided");
+            RuleFor(x => x.Country).NotEmpty().NotNull().WithMessage("Country must be provided");
+            
         }
     }
 }
