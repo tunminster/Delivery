ef9dfa0871381015e4f30938eac83456f4549165
tminhein <tminhein@gmail.com>
2021-09-09 22:51:53 +0100

    fix notification registration

--- Delivery.Notifications/Constants/NotificationHubConstants.cs
+++ Delivery.Notifications/Constants/NotificationHubConstants.cs
@@ -3,6 +3,6 @@ namespace Delivery.Notifications.Constants
     public static class NotificationHubConstants
     {
         public const string NotificationHubConnectionStringName = "Notification-Hub-DefaultFullSharedAccessSignature";
-        public const string NotificationHubName = "Hub-Name";
+        public const string NotificationHubName = "hn-platform-notification-hub-dev";
     }
 }
