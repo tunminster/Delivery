d931c68b22b4e9e3f0bd30c98a4a0b2b76137c06
tminhein <tminhein@gmail.com>
2021-09-20 00:02:04 +0100

    fix notify hub

--- DevOps/Infrastructures/provisions/notification-hub.tf
+++ DevOps/Infrastructures/provisions/notification-hub.tf
@@ -14,7 +14,7 @@ resource "azurerm_notification_hub" "hn-notification-hub" {
   location            = azurerm_resource_group.hn-platform-data-persistent.location
 }
 
-resource "azurerm_notification_hub" "hn-notification-hub" {
+resource "azurerm_notification_hub" "hn-notification-driver-hub" {
   name                = "hn-platform-notification-driver-hub-${var.environment_prefix}"
   namespace_name      = azurerm_notification_hub_namespace.hn-notification-hub-namespace.name
   resource_group_name = azurerm_resource_group.hn-platform-data-persistent.name
