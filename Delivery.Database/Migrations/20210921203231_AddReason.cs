702330d9b349424115eadf2c1f1a969477b71e6c
tminhein <tminhein@gmail.com>
2021-09-21 21:37:37 +0100

    fix reason field

---
+++ Delivery.Database/Migrations/20210921203231_AddReason.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Delivery.Database.Migrations
+{
+    public partial class AddReason : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Reason",
+                table: "DriverOrders",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Reason",
+                table: "DriverOrders");
+        }
+    }
+}
