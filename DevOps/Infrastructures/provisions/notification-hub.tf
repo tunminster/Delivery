a89f8a99c8644124e1c7156dab071b769626e90a
tminhein <tminhein@gmail.com>
2021-09-23 00:44:35 +0100

    add shop hub

--- DevOps/Infrastructures/provisions/notification-hub.tf
+++ DevOps/Infrastructures/provisions/notification-hub.tf
@@ -20,3 +20,10 @@ resource "azurerm_notification_hub" "hn-notification-driver-hub" {
   resource_group_name = azurerm_resource_group.hn-platform-data-persistent.name
   location            = azurerm_resource_group.hn-platform-data-persistent.location
 }
+
+resource "azurerm_notification_hub" "hn-notification-shop-hub" {
+  name                = "hn-platform-notification-shop-hub-${var.environment_prefix}"
+  namespace_name      = azurerm_notification_hub_namespace.hn-notification-hub-namespace.name
+  resource_group_name = azurerm_resource_group.hn-platform-data-persistent.name
+  location            = azurerm_resource_group.hn-platform-data-persistent.location
+}
