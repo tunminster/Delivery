995eb62e7b597895d647d05d3d7b66bb08d47d54
tminhein <tminhein@gmail.com>
2021-09-27 08:50:31 +0100

    add get menu list

---
+++ Delivery.Shop.Domain/Contracts/V1/RestContracts/ShopMenu/ShopMenuStatusContract.cs
@@ -0,0 +1,28 @@
+namespace Delivery.Shop.Domain.Contracts.V1.RestContracts.ShopMenu
+{
+    /// <summary>
+    ///  Shop menu status contract
+    /// </summary>
+    public record ShopMenuStatusContract
+    {
+        /// <summary>
+        ///  Product id
+        /// </summary>
+        public string ProductId { get; init; } = string.Empty;
+
+        /// <summary>
+        ///  Product name
+        /// </summary>
+        public string ProductName { get; init; } = string.Empty;
+
+        /// <summary>
+        ///  Price
+        /// </summary>
+        public int UnitPrice { get; init; }
+        
+        /// <summary>
+        ///  Status
+        /// </summary>
+        public bool Status { get; init; }
+    }
+}
