06cf5c362b13ebcfcc588234478af102cd0054fa
tminhein <tminhein@gmail.com>
2021-10-10 00:16:16 +0100

    add entity

---
+++ Delivery.Database/Migrations/20211009231124_AddOrderPayment.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Delivery.Database.Migrations
+{
+    public partial class AddOrderPayment : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "DriverTransferredId",
+                table: "Orders",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "ShopOwnerTransferredId",
+                table: "Orders",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<int>(
+                name: "StoreOwnerPaymentAmount",
+                table: "Orders",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "DriverTransferredId",
+                table: "Orders");
+
+            migrationBuilder.DropColumn(
+                name: "ShopOwnerTransferredId",
+                table: "Orders");
+
+            migrationBuilder.DropColumn(
+                name: "StoreOwnerPaymentAmount",
+                table: "Orders");
+        }
+    }
+}
