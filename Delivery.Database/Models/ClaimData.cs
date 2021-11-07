c50ef966296dcf6961a0281df213439982290b6c
tminhein <tminhein@gmail.com>
2021-11-07 14:42:17 +0000

    claim data

--- Delivery.Database/Models/ClaimData.cs
+++ Delivery.Database/Models/ClaimData.cs
@@ -7,6 +7,8 @@ namespace Delivery.Database.Models
         public static readonly ClaimData JwtClaimIdentifyClaim = new("Role", "api_access");
         public static readonly ClaimData OrderPageAccess = new ("OrderPageAccess", "Allow");
         public static readonly ClaimData BackEndUserAccess = new ("OrderUpdateAccess", "Allow");
+
+        public static readonly ClaimData AdminUserAccess = new("AdminApiAccess", "admin_api_access");
         public static readonly ClaimData ShopApiAccess = new("ShopApiAccess", "shop_api_access");
         public static readonly ClaimData DriverApiAccess = new("DriverApiAccess", "driver_api_access");
         public static readonly ClaimData CustomerApiAccess = new("CustomerApiAccess", "customer_api_access");
