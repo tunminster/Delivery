900ac2679f394780429278634769cd2cb389d289
tminhein <tminhein@gmail.com>
2021-11-21 15:41:53 +0000

    fix front end url

--- DevOps/Infrastructures/api-management/variables.tf
+++ DevOps/Infrastructures/api-management/variables.tf
@@ -15,3 +15,7 @@ variable "release_name" {
   default     = "1.0.0"
   description = "The release_name number to link which release_name the infratructure was created from."
 }
+
+variable "frontend_url_for_cors"{
+  default = "localhost:5001"
+}
