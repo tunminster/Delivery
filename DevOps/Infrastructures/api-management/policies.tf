a7bd1239b4624776b2150b345953fe6d52ac7953
tminhein <tminhein@gmail.com>
2021-11-21 17:53:11 +0000

    add variable group

--- DevOps/Infrastructures/api-management/policies.tf
+++ DevOps/Infrastructures/api-management/policies.tf
@@ -2,7 +2,7 @@ resource "azurerm_api_management_api_policy" "hn-platform-public-management-api-
   resource_group_name = data.azurerm_api_management.hn-platform.resource_group_name
   api_management_name = data.azurerm_api_management.hn-platform.name
   api_name            = azurerm_api_management_api.hn-platform-public-management-api.name
-  xml_content = templatefile("../api-management/policies/global-policies.xml",
+  xml_content = templatefile("/DevOps/Infrastructures/api-management/policies/global-policies.xml",
     {
       frontend_url           = var.frontend_url_for_cors
   })
