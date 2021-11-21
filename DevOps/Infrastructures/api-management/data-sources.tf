9be2063b8f35476161c29e997ccd88b157ccc2ff
tminhein <tminhein@gmail.com>
2021-11-21 11:23:45 +0000

    fix

--- DevOps/Infrastructures/api-management/data-sources.tf
+++ DevOps/Infrastructures/api-management/data-sources.tf
@@ -1,4 +1,4 @@
 data "azurerm_api_management" "hn-platform" {
-  name                = "hn-apim-${var.environment}"
-  resource_group_name = "hn-platform-data-persistent-${var.environment}"
+  name                = "hn-apim-dev"
+  resource_group_name = "hn-platform-data-persistent-dev"
 }
