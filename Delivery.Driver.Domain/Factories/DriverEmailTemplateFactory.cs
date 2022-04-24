9eec22e3ddeadae32348b47a0da7dbb0c1051ace
Tun Hein <tunhein@Tuns-MacBook-Pro.local>
2022-04-25 00:17:46 +0100

    fix diver config

--- Delivery.Driver.Domain/Factories/DriverEmailTemplateFactory.cs
+++ Delivery.Driver.Domain/Factories/DriverEmailTemplateFactory.cs
@@ -3,47 +3,49 @@ using System.Collections.Generic;
 using Delivery.Azure.Library.Communications.SendGrid.Contracts.V1.RestContracts;
 using Delivery.Driver.Domain.Configurations;
 
-namespace Delivery.Driver.Domain.Factories;
-
-public class DriverEmailTemplateFactory
+namespace Delivery.Driver.Domain.Factories
 {
-    private readonly IServiceProvider serviceProvider;
-
-    public DriverEmailTemplateFactory(IServiceProvider serviceProvider)
+    public class DriverEmailTemplateFactory
     {
-        this.serviceProvider = serviceProvider;
-    }
+        private readonly IServiceProvider serviceProvider;
 
-    public SendGridEmailCreationContract Create(DriverEmailTemplateModel driverEmailTemplateModel)
-    {
-        var driverEmailNotificationConfiguration = new DriverEmailNotificationConfigurationDefinition(serviceProvider);
-        var sendGridEmailCreationContract = new SendGridEmailCreationContract
+        public DriverEmailTemplateFactory(IServiceProvider serviceProvider)
         {
-            Template = new EmailTemplateContract
+            this.serviceProvider = serviceProvider;
+        }
+
+        public SendGridEmailCreationContract Create(DriverEmailTemplateModel driverEmailTemplateModel)
+        {
+            var driverEmailNotificationConfiguration =
+                new DriverEmailNotificationConfigurationDefinition(serviceProvider);
+            var sendGridEmailCreationContract = new SendGridEmailCreationContract
             {
-                TemplateId = driverEmailTemplateModel.TemplateId,
-                DynamicTemplateData = driverEmailTemplateModel.DynamicTemplateData
-            },
-            InitiatedOn = DateTimeOffset.UtcNow,
-            NotificationSendAt = DateTimeOffset.UtcNow,
-            BusinessDomainType = driverEmailTemplateModel.BusinessDomainType.ToString(),
-            EmailSender = new EmailContract
-            {
-                EmailAddress = driverEmailNotificationConfiguration.SenderEmail,
-                Name = "Ragibull Driver Partner"
-            },
-            EmailReplyTo = new EmailContract
-            {
-                EmailAddress = driverEmailNotificationConfiguration.ReplyEmail,
-                Name = "No-Reply"
-            },
-            Categories = new List<string>
-            {
-                driverEmailTemplateModel.BusinessDomainType.ToString(), "email_notification"
-            },
-            Subject = driverEmailTemplateModel.Subject,
-            NotificationUniqueId = driverEmailTemplateModel.NotificationUniqueId
-        };
-        return sendGridEmailCreationContract;
+                Template = new EmailTemplateContract
+                {
+                    TemplateId = driverEmailTemplateModel.TemplateId,
+                    DynamicTemplateData = driverEmailTemplateModel.DynamicTemplateData
+                },
+                InitiatedOn = DateTimeOffset.UtcNow,
+                NotificationSendAt = DateTimeOffset.UtcNow,
+                BusinessDomainType = driverEmailTemplateModel.BusinessDomainType.ToString(),
+                EmailSender = new EmailContract
+                {
+                    EmailAddress = driverEmailNotificationConfiguration.SenderEmail,
+                    Name = "Ragibull Driver Partner"
+                },
+                EmailReplyTo = new EmailContract
+                {
+                    EmailAddress = driverEmailNotificationConfiguration.ReplyEmail,
+                    Name = "No-Reply"
+                },
+                Categories = new List<string>
+                {
+                    driverEmailTemplateModel.BusinessDomainType.ToString(), "email_notification"
+                },
+                Subject = driverEmailTemplateModel.Subject,
+                NotificationUniqueId = driverEmailTemplateModel.NotificationUniqueId
+            };
+            return sendGridEmailCreationContract;
+        }
     }
 }
