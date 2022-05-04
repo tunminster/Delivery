040c44dc8afa92084acb368e2247c092b846f1f4
Tun Hein <tunhein@Tuns-MacBook-Pro.local>
2022-05-04 10:35:08 +0100

    fix gateway

--- DevOps/Infrastructures/api-management/apis.tf
+++ DevOps/Infrastructures/api-management/apis.tf
@@ -39,3 +39,24 @@ resource "azurerm_api_management_api" "hn-platform-public-customer-api" {
     #content_value  = var.platform_public_management_api_link
   }
 }
+
+resource "azurerm_api_management_api" "hn-platform-public-webapp-api" {
+  name                = "hn-platform-public-webapp-api-dev"
+  resource_group_name = data.azurerm_api_management.hn-platform.resource_group_name
+  api_management_name = data.azurerm_api_management.hn-platform.name
+  revision            = replace(var.release_name, ".", "")
+  display_name        = "Ragibull - Webapp Apis"
+  description         = "Contains all of the webapp endpoints."
+  path                = "webapp"
+  protocols           = [var.protocol]
+  service_url         = "https://delivery-api.harveynetwork.com"
+  #service_url         = "${var.api_backend_url}"
+
+  subscription_required = true
+
+  import {
+    content_format = "openapi-link"
+    content_value = "https://delivery-api.harveynetwork.com/swagger/api/webapp/v1/swagger.json"
+    #content_value  = var.platform_public_management_api_link
+  }
+}
