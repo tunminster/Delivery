d90a844adf69e85c4ec4b68abaa828cdd796f1e5
Tun Hein <tminhein@gmail.com>
2022-10-09 09:40:54 -0700

    fix driver by name controller

--- Delivery.Driver.Domain/Contracts/V1/RestContracts/DriverSearch/DriverSearchByNameContract.cs
+++ Delivery.Driver.Domain/Contracts/V1/RestContracts/DriverSearch/DriverSearchByNameContract.cs
@@ -1,19 +1,20 @@
-namespace Delivery.Driver.Domain.Contracts.V1.RestContracts.DriverSearch;
-
-public record DriverSearchByNameContract
+namespace Delivery.Driver.Domain.Contracts.V1.RestContracts.DriverSearch
 {
-    /// <summary>
-    ///  Free text search
-    /// </summary>
-    public string FreeTextSearch { get; init; } = string.Empty;
-        
-    /// <summary>
-    ///  Current page
-    /// </summary>
-    public int Page { get; init; }
-        
-    /// <summary>
-    ///  Page size
-    /// </summary>
-    public int PageSize { get; init; }
+    public record DriverSearchByNameContract
+    {
+        /// <summary>
+        ///  Free text search
+        /// </summary>
+        public string FreeTextSearch { get; init; } = string.Empty;
+
+        /// <summary>
+        ///  Current page
+        /// </summary>
+        public int Page { get; init; }
+
+        /// <summary>
+        ///  Page size
+        /// </summary>
+        public int PageSize { get; init; }
+    }
 }
