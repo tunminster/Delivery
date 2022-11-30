774d37eb9f31f167883a80234d292cc56e4dc747
Tun Hein <tminhein@gmail.com>
2022-11-30 15:46:15 -0800

    fix namespace

--- Delivery.Managements.Domain/Validators/MeatOptions/MeatOptionCreationValidator.cs
+++ Delivery.Managements.Domain/Validators/MeatOptions/MeatOptionCreationValidator.cs
@@ -2,14 +2,18 @@ using Delivery.Database.Enums;
 using Delivery.Managements.Domain.Contracts.V1.RestContracts.MeatOptions;
 using FluentValidation;
 
-namespace Delivery.Managements.Domain.Validators.MeatOptions;
-
-public class MeatOptionCreationValidator : AbstractValidator<MeatOptionCreationContract>
+namespace Delivery.Managements.Domain.Validators.MeatOptions
 {
-    public MeatOptionCreationValidator()
+    public class MeatOptionCreationValidator : AbstractValidator<MeatOptionCreationContract>
     {
-        RuleFor(x => x.ProductId).NotEmpty().NotNull().WithMessage($"{nameof(MeatOptionCreationContract.ProductId)} must be provided");
-        RuleFor(x => x.MeatOptionText).NotEmpty().NotNull().WithMessage($"{nameof(MeatOptionCreationContract.MeatOptionText)} must be provided");
-        RuleFor(x => x.OptionControlType).NotEqual(OptionControlType.None).WithMessage($"{nameof(MeatOptionCreationContract.OptionControlType)} must not be {nameof(OptionControlType.None)}");
+        public MeatOptionCreationValidator()
+        {
+            RuleFor(x => x.ProductId).NotEmpty().NotNull()
+                .WithMessage($"{nameof(MeatOptionCreationContract.ProductId)} must be provided");
+            RuleFor(x => x.MeatOptionText).NotEmpty().NotNull()
+                .WithMessage($"{nameof(MeatOptionCreationContract.MeatOptionText)} must be provided");
+            RuleFor(x => x.OptionControlType).NotEqual(OptionControlType.None).WithMessage(
+                $"{nameof(MeatOptionCreationContract.OptionControlType)} must not be {nameof(OptionControlType.None)}");
+        }
     }
 }
