d08330e8cb93de02e76cc5c68b4e3dd2fe665f44
Tun Hein <tminhein@gmail.com>
2022-12-05 13:27:36 -0800

    fix .net 6 error

--- Delivery.Managements.Domain/Validators/Coupon/CouponManagementCreationValidator.cs
+++ Delivery.Managements.Domain/Validators/Coupon/CouponManagementCreationValidator.cs
@@ -1,12 +1,14 @@
 using Delivery.Managements.Domain.Contracts.V1.RestContracts.Coupon;
 using FluentValidation;
 
-namespace Delivery.Managements.Domain.Validators.Coupon;
-
-public class CouponManagementCreationValidator : AbstractValidator<CouponManagementCreationContract>
+namespace Delivery.Managements.Domain.Validators.Coupon
 {
-    public CouponManagementCreationValidator()
+    public class CouponManagementCreationValidator : AbstractValidator<CouponManagementCreationContract>
     {
-        RuleFor(x => x.Name).NotEmpty().NotNull().WithMessage($"{nameof(CouponManagementCreationContract.Name)} must be provided.");
+        public CouponManagementCreationValidator()
+        {
+            RuleFor(x => x.Name).NotEmpty().NotNull()
+                .WithMessage($"{nameof(CouponManagementCreationContract.Name)} must be provided.");
+        }
     }
 }
